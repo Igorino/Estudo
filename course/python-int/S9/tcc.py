8df8d5ffa2d3e91490e1ab726d7b4cff960f67c7
Igor Ferreira <igornvf@gmail.com>
2023-03-17 12:59:51 -0300

    Mudança de local dos comentários para melhor visualização

--- course/python-int/S9/tcc.py
+++ course/python-int/S9/tcc.py
@@ -1,7 +1,7 @@
 import re
 
+'''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
 def le_assinatura():
-    '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
     print("Bem-vindo ao detector automático de COH-PIAH.")
     print("Informe a assinatura típica de um aluno infectado:")
 
@@ -14,8 +14,8 @@ def le_assinatura():
 
     return [wal, ttr, hlr, sal, sac, pal]
 
+'''A funcao le todos os textos a serem comparados e devolve uma lista contendo cada texto como um elemento'''
 def le_textos():
-    '''A funcao le todos os textos a serem comparados e devolve uma lista contendo cada texto como um elemento'''
     i = 1
     textos = []
     texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
@@ -26,23 +26,23 @@ def le_textos():
 
     return textos
 
+'''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
 def separa_sentencas(texto):
-    '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
     sentencas = re.split(r'[.!?]+', texto)
     if sentencas[-1] == '':
         del sentencas[-1]
     return sentencas
 
+'''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
 def separa_frases(sentenca):
-    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
     return re.split(r'[,:;]+', sentenca)
 
+'''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
 def separa_palavras(frase):
-    '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
     return frase.split()
 
+'''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
 def n_palavras_unicas(lista_palavras):
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
     freq = dict()
     unicas = 0
     for palavra in lista_palavras:
@@ -57,8 +57,8 @@ def n_palavras_unicas(lista_palavras):
 
     return unicas
 
+'''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
 def n_palavras_diferentes(lista_palavras):
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
     freq = dict()
     for palavra in lista_palavras:
         p = palavra.lower()
@@ -71,10 +71,12 @@ def n_palavras_diferentes(lista_palavras):
 
 def compara_assinatura(as_a, as_b):
     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
+
     pass
 
 def calcula_assinatura(texto):
     '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+
     pass
 
 def avalia_textos(textos, ass_cp):
