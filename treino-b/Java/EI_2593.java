a16b6ee969702ba33fd5d9716e6f44614ebbd6f8
Igor Ferreira <33945122+Igorino@users.noreply.github.com>
2023-03-17 00:39:54 -0300

    Remoção de comentário

--- treino-b/Java/EI_2593.java
+++ treino-b/Java/EI_2593.java
@@ -2,13 +2,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
- 
-/**
- * IMPORTANT: 
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
+
 public class EI_2593 {
 
     static Scanner scanner = new Scanner(System.in);
